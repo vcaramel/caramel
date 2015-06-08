d9bc572df440b1e2088262ab5431115dd1ba85a5
viet <viet.do@yatedo.com>
2015-06-08 09:52:50 +0200

    @

---
+++ public/js/apps/caramel.js
@@ -0,0 +1,5 @@
+/**
+ * Created by viet on 05/06/15.
+ */
+var app = angular.module('caramel', []);
+alert('caramel app');
---
+++ public/js/apps/login.js
@@ -0,0 +1,5 @@
+/**
+ * Created by viet on 05/06/15.
+ */
+var app = angular.module('login', []);
+alert('login app');
